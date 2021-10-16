720537b73ad9a4e4ef05fad5c6d38403c6dabecc
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-10-16 12:05:54 -0300

    criacao da rota Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Header</p>
-      </header>
+      <Header />
 
       <div className="App-main">
         <p>Content</p>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
-
+      
       <footer className="App-footer">
         <p>Footer</p>
       </footer>
     </div>
   );
 }
-
 export default App;
